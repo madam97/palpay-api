726f84b480eae1d2600c659c1de2e90c68da3ad0
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-05 16:45:09 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req,res) => {
+  res.json({ msg: 'Working' });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
