2402b93e84c44944ee94157172be5846ffded30c
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-05 21:12:36 +0100

    changed test logs in index.ts

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config({ path: `.env.${process.env.NODE_ENV}` });
-console.log(`|.env.${process.env.NODE_ENV}|`, process.env);
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 1111;
 app.get('/', (req, res) => {
-    res.json({ msg: 'Working' });
+    res.json({ msg: 'Working', port: process.env.PORT });
 });
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-console.log(`|.env.${process.env.NODE_ENV}|`, process.env);
-
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 1111;
 
 app.get('/', (req,res) => {
-  res.json({ msg: 'Working' });
+  res.json({ msg: 'Working', port: process.env.PORT });
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
