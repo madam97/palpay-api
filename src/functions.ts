dfddc68c2da2e4cc43a73ba0e40b9977cc84267c
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-06 22:48:22 +0100

    added camel, pascal and snake case changer functions

---
+++ src/functions.ts
@@ -0,0 +1,44 @@
+/// STRING
+
+export function ucfirst(text: string): string {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+export function toSnakeCase(text: string): string {
+  return text.split(/(?=[A-Z])/).join('_').toLowerCase();
+}
+
+export function toCamelCase(text: string): string {
+  return /[-_]/g.test(text) ? text.toLowerCase().replace(/[-_][a-z0-9]/g, (group) => group.slice(-1).toUpperCase()) : text;
+}
+
+export function toPascalCase(text: string): string {
+  return ucfirst( toCamelCase(text) );
+}
+
+
+/// OBJECT
+
+const helperCaseObject = (data: object, caseFunc: Function): object => {
+  const newData: any[][] = [];
+
+  Object.entries(data).forEach(([key, value]) => {
+    console.log(key, '->', caseFunc(key.toString()), caseFunc);
+
+    newData.push([ caseFunc(key.toString()), value ]);
+  });
+
+  return Object.fromEntries(newData);
+}
+
+export function toSnakeCaseObject(data: object): object {
+  return helperCaseObject(data, toSnakeCase);
+}
+
+export function toCamelCaseObject(data: object): object {
+  return helperCaseObject(data, toCamelCase);
+}
+
+export function toPascalCaseObject(data: object): object {
+  return helperCaseObject(data, toPascalCase);
+}
