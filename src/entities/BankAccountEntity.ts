8436f29e8a1c48e171ca5c155439f071b7e3373b
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-09 21:52:58 +0100

    changed Entity to use dynamic property, BankAccountEntity is using the new Entity class added toObject method, NAME readonly property to Entity

--- src/entities/BankAccountEntity.ts
+++ src/entities/BankAccountEntity.ts
@@ -1,29 +1,19 @@
 import Entity from './Entity';
 import IObject from '../interfaces/IObject';
-import { toCamelCaseObject } from '../functions';
 
 export default class BankAccountEntity extends Entity {
-  id: number | null;
-  accountNumber: string;
-  balance: number;
-
   constructor(data: IObject) {
-    super();
-
-    data = toCamelCaseObject(data);
-
-    this.id = data.id ?? null;
-    this.accountNumber = data.accountNumber ?? '';
-    this.balance = data.balance ?? 0;
-  }
-
-  static get NAME() { return 'BankAccount'; }
-
-  public toArray(): any[] {
-    return [this.accountNumber, this.balance, this.id];
-  }
-
-  public toArrayNoId(): any[] {
-    return [this.accountNumber, this.balance];
+    super(data, {
+      accountNumber: {
+        type: 'string',
+        required: true,
+        default: ''
+      },
+      balance: {
+        type: 'number',
+        required: false,
+        default: 0
+      }
+    });
   }
 }
