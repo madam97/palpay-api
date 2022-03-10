dc9cc7899d4f110ecf91a3ea6f95d118cb9fbe40
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-10 15:58:44 +0100

    changed Repository's abstract methods to use generics and defined them in the class added Repository NAME property removed abstract methods definition from BankAccountRepository

--- src/repositories/BankAccountRepository.ts
+++ src/repositories/BankAccountRepository.ts
@@ -10,38 +10,4 @@ export default class BankAccountRepository extends Repository<BankAccountEntity>
   public getEntity(data: object): BankAccountEntity {
     return new BankAccountEntity(data);
   }
-
-  public getEntitiesData(entities: BankAccountEntity[]): object[] {
-    const data: object[] = [];
-    entities.map(entity => data.push( entity.toObject() ));
-    return data;
-  }
-
-  public async find(): Promise<BankAccountEntity[]> {
-    const entities: BankAccountEntity[] = [];
-
-    const result = await this.db.select('BankAccount/selectAll');
-    result.map(row => entities.push( this.getEntity(row) ));
-
-    return entities;
-  }
-
-  public async findOne(id: number): Promise<BankAccountEntity> {
-    const result = await this.db.selectOne('BankAccount/selectOne', id);
-    return this.getEntity(result);
-  }
-
-  public async create(entity: BankAccountEntity): Promise<BankAccountEntity> {
-    entity.id = await this.db.insert('BankAccount/insertOne', entity.toArray(true));
-
-    return entity;
-  }
-
-  public async update(entity: BankAccountEntity): Promise<boolean> {
-    return await this.db.update('BankAccount/updateOne', entity.toArray());
-  }
-
-  public async delete(id: number): Promise<boolean> {
-    return await this.db.delete('BankAccount/deleteOne', id);
-  }
 }
--- src/repositories/Repository.ts
+++ src/repositories/Repository.ts
@@ -1,23 +1,49 @@
 import Database from '../Database';
+import Entity from '../entities/Entity';
 
-export default abstract class Repository<T> {
+export default abstract class Repository<T extends Entity> {
+  protected NAME: string;
   protected db: Database;
+  protected entityName: string;
 
   constructor(db: Database) {
+    this.NAME = new.target.name.replace(/Repository$/i, '');
     this.db = db;
   }
 
   public abstract getEntity(data: object): T;
 
-  public abstract getEntitiesData(entities: T[]): object[];
+  public getEntitiesData(entities: T[]): object[] {
+    const data: object[] = [];
+    entities.map(entity => data.push( entity.toObject() ));
+    return data;
+  }
 
-  public abstract find(): Promise<T[]>;
+  public async find(): Promise<T[]> {
+    const entities: T[] = [];
 
-  public abstract findOne(id: number): Promise<T>;
+    const result = await this.db.select(`${this.NAME}/selectAll`);
+    result.map(row => entities.push( this.getEntity(row) ));
 
-  public abstract create(entity: T): Promise<T>;
+    return entities;
+  }
 
-  public abstract update(entity: T): Promise<boolean>;
+  public async findOne(id: number): Promise<T> {
+    const result = await this.db.selectOne(`${this.NAME}/selectOne`, id);
+    return this.getEntity(result);
+  }
 
-  public abstract delete(id: number): Promise<boolean>;
+  public async create(entity: T): Promise<T> {
+    entity.id = await this.db.insert(`${this.NAME}/insertOne`, entity.toArray(true));
+
+    return entity;
+  }
+
+  public async update(entity: T): Promise<boolean> {
+    return await this.db.update(`${this.NAME}/updateOne`, entity.toArray());
+  }
+
+  public async delete(id: number): Promise<boolean> {
+    return await this.db.delete(`${this.NAME}/deleteOne`, id);
+  }
 }
