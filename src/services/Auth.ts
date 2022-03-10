f5775579d165975e625d95fb38f507ec88229e44
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-10 18:29:50 +0100

    added jsonwebtoken package and Auth class

---
+++ src/services/Auth.ts
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+export default class Auth {
+  private SECRET: string;
+  private config: object;
+
+  constructor(SECRET: string, config: object = {}) {
+    this.SECRET = SECRET;
+    this.config = config;
+  }
+
+  public login(payload: object): string {
+    return jwt.sign(payload, this.SECRET, this.config);
+  }
+
+  public verifyToken(authHeader: string): object {
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+      throw new Error('authorization header is missing');
+    }
+
+    const payload = jwt.verify(token, this.SECRET);
+
+    return payload;
+  }
+}
