6b673134ed08b2147b657b13a94ab0200e98048c
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-11 20:37:15 +0100

    changed setRoutes to store auth object, set default routes config and use this config to add dynamically API endpoints added setRoutes and functions for every endpoint methods

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,12 @@
 import Database from '../Database';
+import Auth from '../services/Auth';
 import Controller from './Controller';
 import UserEntity from '../entities/UserEntity';
 import UserRepository from '../repositories/UserRepository';
 
 export default class UserController extends Controller<UserEntity> {
-  constructor(db: Database) {
-    super(db);
+  constructor(db: Database, auth: Auth) {
+    super(db, auth);
     this.repo = new UserRepository(db);
 
     super.setRoutes();
