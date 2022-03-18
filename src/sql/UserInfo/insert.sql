6377a870cac33c92f8583d9f0471136c74743b9b
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-18 20:35:32 +0100

    added UserInfo sql

---
+++ src/sql/UserInfo/insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO `{PREFIX}user_infos` (user_id, name, address, telephone, email) VALUES
+(?,?,?,?,?);
---
+++ src/sql/UserInfo/update.sql
@@ -0,0 +1,3 @@
+UPDATE `{PREFIX}user_infos` 
+SET user_id = ?, name = ?, address = ?, telephone = ?, email = ?
+WHERE id = ?;
