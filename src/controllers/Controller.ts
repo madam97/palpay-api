726afb3df698dbe0c4470d14b6462b81f885bc0d
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-18 21:04:44 +0100

    added base endpoint methods, generic usage, default route function set to Controller

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Model, Entity } from '../models/Model';
 import TMethod from '../types/TMethod';
 
 export interface Route {
@@ -6,21 +7,115 @@ export interface Route {
   path: string,
   role?: string,
   verifyUserId?: boolean,
-  func: Function
+  func?: Function
 };
 
-export class Controller {
+export class Controller<T extends Entity> {
 
   readonly router: express.Router;
-  private routes: Route[];
+  private routes: Route[] = [];
+  protected model: Model<T>;
 
-  constructor() {
+  constructor(model: Model<T>) {
     this.router = express.Router();
+    this.model = model;
   }
 
-  protected async runRoute(req: express.Request, res: express.Response): Promise<void> {
+
+  /// ROUTE METHODS
+
+  /**
+   * Saves and uses multiple routes' config
+   * @param routes 
+   */
+  protected setRoutes(routes: Route[]) {
+    routes.map(route => {
+      this.addRoute(route);
+    });
+  }
+
+  /**
+   * Saves the route config and add a new express route using the config
+   * @param route 
+   */
+  protected addRoute(route: Route): void {
+    // Save route config
+    if (!route.func) {
+      switch (route.method) {
+        case 'GET':     route.func = route.path === '/:id' ? this.getOne.bind(this) : this.get.bind(this); break;
+        case 'POST':    route.func = this.post.bind(this); break;
+        case 'PUT':     route.func = this.put.bind(this); break;
+        case 'PATCH':   route.func = this.patch.bind(this); break;
+        case 'DELETE':  route.func = this.delete.bind(this); break;
+      }
+    }
+
+    this.routes.push(route);
+
+    // Handlers
+    const handlers: express.RequestHandler[] = [];
+    // if (route.role) {
+    //   handlers.push( this.verifyRoute.bind(this) );
+    // }
+    // if (route.verifyUserId) {
+    //   handlers.push( this.verifyUserId.bind(this) );
+    // }
+
+    // Endpoint run with handlers
+    if (handlers.length > 0) {
+      switch (route.method) {
+        case 'GET':     this.router.get(route.path, ...handlers, this.runEndpoint.bind(this)); break;
+        case 'POST':    this.router.post(route.path, ...handlers, this.runEndpoint.bind(this)); break;
+        case 'PUT':     this.router.put(route.path, ...handlers, this.runEndpoint.bind(this)); break;
+        case 'PATCH':   this.router.patch(route.path, ...handlers, this.runEndpoint.bind(this)); break;
+        case 'DELETE':  this.router.delete(route.path, ...handlers, this.runEndpoint.bind(this)); break;
+      }
+    }
+    
+    // Endpoint run without handlers
+    else {
+      switch (route.method) {
+        case 'GET':     this.router.get(route.path, this.runEndpoint.bind(this)); break;
+        case 'POST':    this.router.post(route.path, this.runEndpoint.bind(this)); break;
+        case 'PUT':     this.router.put(route.path, this.runEndpoint.bind(this)); break;
+        case 'PATCH':   this.router.patch(route.path, this.runEndpoint.bind(this)); break;
+        case 'DELETE':  this.router.delete(route.path, this.runEndpoint.bind(this)); break;
+      }
+    }
+  }
+
+  /**
+   * Returns the route config of the given method and path
+   * @param method 
+   * @param path 
+   * @returns 
+   */
+  protected getRoute(method: string, path: string): Route {
+    const route = this.routes.find(route => route.method === method && route.path === path);
+
+    if (!route) {
+      throw new Error(`Cannot ${method} ${path}`);
+    }
+
+    return route;
+  }
+
+  
+
+  /// ENDPOINTS METHODS
+
+  /**
+   * Runs the endpoint of the given request
+   * @param req 
+   * @param res 
+   */
+  protected async runEndpoint(req: express.Request, res: express.Response): Promise<void> {
     try {
       const route = this.getRoute(req.method, req.route.path);
+
+      if (!route.func) {
+        throw new Error(`${req.method} ${req.route.path} do not have function`);
+      }
 
       const args = [];
       if (req.params.id) {
@@ -38,50 +133,55 @@ export class Controller {
     }
   }
 
-  protected setRoutes(routes: Route[]) {
-    this.routes = routes;
+  // GET /
+  protected async get(): Promise<object[]> {
+    const entities = await this.model.find();
 
-    this.routes.map(route => {
-      this.setRoute(route);
-    });
+    return this.model.toObjectArray(entities);
   }
 
-  protected setRoute(route: Route): void {
-    const handlers: express.RequestHandler[] = [];
-    // if (route.role) {
-    //   handlers.push( this.verifyRoute.bind(this) );
-    // }
-    // if (route.verifyUserId) {
-    //   handlers.push( this.verifyUserId.bind(this) );
-    // }
+  // GET /:id
+  protected async getOne(id: number): Promise<object> {
+    const entity = await this.model.findOne(id);
 
-    if (handlers.length > 0) {
-      switch (route.method) {
-        case 'GET':     this.router.get(route.path, ...handlers, this.runRoute.bind(this)); break;
-        case 'POST':    this.router.post(route.path, ...handlers, this.runRoute.bind(this)); break;
-        case 'PUT':     this.router.put(route.path, ...handlers, this.runRoute.bind(this)); break;
-        case 'PATCH':   this.router.patch(route.path, ...handlers, this.runRoute.bind(this)); break;
-        case 'DELETE':  this.router.delete(route.path, ...handlers, this.runRoute.bind(this)); break;
-      }
-    } else {
-      switch (route.method) {
-        case 'GET':     this.router.get(route.path, this.runRoute.bind(this)); break;
-        case 'POST':    this.router.post(route.path, this.runRoute.bind(this)); break;
-        case 'PUT':     this.router.put(route.path, this.runRoute.bind(this)); break;
-        case 'PATCH':   this.router.patch(route.path, this.runRoute.bind(this)); break;
-        case 'DELETE':  this.router.delete(route.path, this.runRoute.bind(this)); break;
-      }
-    }
+    return this.model.toObject(entity);
   }
 
-  protected getRoute(method: string, path: string): Route {
-    const route = this.routes.find(route => route.method === method && route.path === path);
+  // POST /
+  protected async post(body: object): Promise<object> {
+    const entity = this.model.format(body);
 
-    if (!route) {
-      throw new Error(`Cannot ${method} ${path}`);
-    }
+    const newEntity = await this.model.create(entity);
 
-    return route;
+    return this.model.toObject(newEntity);
+  }
+
+  // PUT /:id
+  protected async put(id: number, body: object): Promise<object> {
+    const entity = this.model.format({ ...body, id });
+
+    await this.model.update(entity);
+
+    return this.model.toObject(entity);
+  }
+
+  // PATCH /:id
+  protected async patch(id: number, body: object): Promise<object> {
+    const entity = await this.model.findOne(id);
+    const newEntity = this.model.format({ ...entity, ...body, id });
+
+    await this.model.update(newEntity);
+
+    return this.model.toObject(newEntity);
+  }
+
+  // DELETE /:id
+  protected async delete(id: number): Promise<object> {
+    const entity = await this.model.findOne(id);
+    
+    await this.model.delete(id);
+
+    return this.model.toObject(entity);
   }
 
 }
