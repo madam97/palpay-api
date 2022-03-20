2c29aaae935b5c490a9fcd9f668a52d33d59d3f1
Mézes Ádám <mezesadam1997@gmail.com>
2022-03-20 20:04:42 +0100

    added UserController and its usage

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import config from './config';
 import { authRouter } from './controllers/AuthController';
+import { userRouter } from './controllers/UserController';
 import { userInfoRouter } from './controllers/UserInfoController';
 
 async function main(): Promise<void> {
@@ -11,6 +12,7 @@ async function main(): Promise<void> {
 
   // Routes
   app.use('/api/auth', authRouter);
+  app.use('/api/user', userRouter);
   app.use('/api/user-info', userInfoRouter);
   
   app.listen(config.PORT, () => console.log(`Server started on port ${config.PORT}`));
